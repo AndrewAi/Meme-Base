24e66d051a4a21bd86b9a89bd28395e481ca9f1a
AndrewAi <andrewiriwin@gmail.com>
2016-11-24 20:42:37 +0000

    V3 3.2.1 I can now retrieve from couchdb store in python var then pass var to be displayed in html

--- app.py
+++ app.py
@@ -8,17 +8,18 @@ app = Flask(__name__)
 
 couch = couchdb.Server('http://127.0.0.1:5984/')
 
-db = couch.create('test2')
+db = couch['test2']
 
-doc = {'Irwin': 'Irwin'}
-db.save(doc)
+a = db['af0a87fa1f89e2a0d6f56b429d0012e3']
+#db['123'] = {'irwin','andrew'} 
 
+#print("a : ", a["Andy"] )
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', a = a['Andy'])
 
 
 if __name__ == '__main__':
