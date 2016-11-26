25a2226321f737e3bab9b043d68ec5a3c6464245
AndrewAi <andrewiriwin@gmail.com>
2016-11-26 02:30:23 +0000

    v3 3.2.2 2:28am commit :P I can now display an image stored in couchdb using http instead of previously imported couchdb package

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request,render_template,flash,request,url_for,redirect
+from flask import Flask, request, render_template, flash, request, url_for, redirect
 import couchdb
+import requests
 
 
 
@@ -10,23 +11,50 @@ couch = couchdb.Server('http://127.0.0.1:5984/')
 
 db = couch['test2']
 
-a = db['af0a87fa1f89e2a0d6f56b429d0012e3']
+#a = db['af0a87fa1f89e2a0d6f56b429d0012e3']
+#r = requests.get('http://127.0.0.1:5984/_all_dbs')
+#print("r:", r)
 #a = db.get_attachment('af0a87fa1f89e2a0d6f56b429d0012e3','aboutme.jpg')
 #db['123'] = {'irwin','andrew'} 
+#print("a : ", a["Andy"] )
 
-print("a : ", a["Andy"] )
+#j = {"andy":"irw"}
+#requests.post('http://127.0.0.1:5984/test2/',data=None,json=j)
+doc = requests.get('http://localhost:5984/test2/2/aboutme.jpg')
+
+
+#doc = "http://localhost:5984/test2/2/aboutme.jpg"
+print("doc: ", doc.url)
+
+
+#doc = db.get_attachment('2', 'aboutme.jpg', default=None)
+
+#print("doc: ", doc)
 
 
 
 @app.route('/')
 def index():
-    return render_template('index.html', a = a)
+    return render_template('index.html', doc = doc)
+
+
+
+@app.route('/imgurl/')
+def imgurl():
+    return render_template('imgurl.html', doc = doc.url)
+
+
+
+
 
 @app.route('/page/')
 def page():
 
     return("Andrew Irwin")    
-    #return render_template('index.html', a = a)
+    #return render_template('index.html')
+
+
+
 
 @app.route('/test/', methods=["GET","POST"])
 def test():
@@ -37,6 +65,7 @@ def test():
     if request.method == "POST":
         print("posted")
         
+
          
         
 
